3bd009f61db2e66647d07e6eb8f4076ab6002221
Ryota Tomiyama <lit.karu.noir@gmail.com>
2018-09-16 01:51:31 +0900

    finished kadai1

--- app/src/main/java/jp/techacademy/tomiyama/ryota/aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/tomiyama/ryota/aisatsuapp/MainActivity.java
@@ -1,13 +1,64 @@
 package jp.techacademy.tomiyama.ryota.aisatsuapp;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView textView;
+    Calendar cal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        cal = Calendar.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button){
+            showTimePickerDialog();
+        }
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                        Log.d("UI-PARTS",String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                        if( 2 <= hourOfDay && hourOfDay < 10){
+                            textView.setText("おはよう");
+                        } else if (10 <= hourOfDay && hourOfDay < 18) {
+                            textView.setText("こんにちは");
+                        } else {
+                            textView.setText("こんばんは");
+                        }
+                    }
+                },
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                true);
+
+        timePickerDialog.show();
     }
 }
